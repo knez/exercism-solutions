c0f97dc2d50cafa0057c71e1e52268b1a826a742
Nikola Knežević <48497634+knez@users.noreply.github.com>
2020-05-28 13:53:23 +0200

    Refactor is_leap_year inside a header

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -2,9 +2,4 @@
 
 namespace leap {
 
-bool is_leap_year(int year)
-{
-    return !(year % 4) && (year % 100 || !(year % 400));
-}
-
 }  // namespace leap
--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -3,7 +3,10 @@
 
 namespace leap {
 
-    bool is_leap_year(int year);
+    inline bool is_leap_year(int year)
+    {
+         return !(year % 4) && (year % 100 || !(year % 400));
+    }
 
 }  // namespace leap
 
