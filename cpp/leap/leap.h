1443df638809be10addd7639aaf00b35496af103
Nikola Knežević <48497634+knez@users.noreply.github.com>
2020-05-29 22:15:29 +0200

    Refactor for better readability

--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -3,10 +3,10 @@
 
 namespace leap {
 
-    inline bool is_leap_year(int year)
-    {
-         return !(year % 4) && (year % 100 || !(year % 400));
-    }
+inline bool is_leap_year(int year)
+{
+     return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
 
 }  // namespace leap
 
