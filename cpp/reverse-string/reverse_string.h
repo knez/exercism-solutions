6021b4380416737f5b1920daac081d427dcdbc4e
Nikola Knežević <48497634+knez@users.noreply.github.com>
2020-05-29 14:36:34 +0200

    Refactor using only built-in methods

--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -5,7 +5,10 @@
 
 namespace reverse_string {
 
-    std::string reverse_string(std::string str);
+inline std::string reverse_string(std::string str)
+{
+    return std::string(str.rbegin(), str.rend());
+}
 
 }  // namespace reverse_string
 
