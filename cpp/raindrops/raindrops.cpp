14c01b300f5005351cffec6e362abf14a53a2840
Nikola Knežević <48497634+knez@users.noreply.github.com>
2020-06-14 16:56:56 +0200

    Refactor raindrops

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -6,14 +6,14 @@ namespace raindrops {
 
 std::string convert(int num)
 {
-    std::stringstream str;
+    std::ostringstream str;
 
     if (num % 3 == 0) str << "Pling";
     if (num % 5 == 0) str << "Plang";
     if (num % 7 == 0) str << "Plong";
 
     // if str empty, add digits
-    if (str.rdbuf()->in_avail() == 0)
+    if (str.tellp() == 0)
         str << std::to_string(num);
 
     return str.str();
